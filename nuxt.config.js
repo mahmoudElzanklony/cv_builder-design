fb439a43898c56da609c824e9307f25851b55926
mahmoud_elzanklony <mahmoud_elzanklony@yahoo.com>
2024-03-19 03:43:32 +0200

    api url

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,7 +110,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
      //baseURL: 'http://localhost:8000/api/',
-     baseURL: 'https://cvapi.skillar.com/',
+     baseURL: 'https://cvapi.skillar.com/api/',
     // proxy: true,
   },
 
